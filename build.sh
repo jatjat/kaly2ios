b159a7125ad8526442c7de415d39db6fcb2e359b
Joel Thiessen <jatjat@users.noreply.github.com>
2022-09-05 01:13:10 -0500

    Try different path vars

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # on CI:
-BREW=/usr/local/bin/brew
-BAZELISK=/usr/local/bin/bazelisk
-MINT=/usr/local/bin/mint
+BREW_CMD=/usr/local/bin/brew
+BAZELISK_CMD=/usr/local/bin/bazelisk
+MINT_CMD=/usr/local/bin/mint
 
 # # on local:
 # BREW=/opt/homebrew/bin/brew
-# BAZELISK=/opt/homebrew/bin/bazelisk
-# MINT=/opt/homebrew/bin/mint
+# BAZELISK=./opt/homebrew/bin/bazelisk
+# MINT=./opt/homebrew/bin/mint
 
 
 
 
-which brew && which mint && export PATH="" && $BREW install mint && $MINT bootstrap && $BAZELISK build xcodeproj && $BAZELISK build ios-app
+which brew && which mint && export PATH="" && /usr/local/bin/brew install mint && /usr/local/bin/mint bootstrap && /usr/local/bin/bazelisk build xcodeproj && /usr/local/bin/bazelisk build ios-app
