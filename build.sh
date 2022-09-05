b0d2e44893597be1d74655863b05b13e9d1139ed
Joel Thiessen <jatjat@users.noreply.github.com>
2022-09-05 01:00:55 -0500

    Try with PATH="" and substitute paths

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-brew install mint && mint bootstrap && bazelisk build xcodeproj && bazelisk build ios-app
+# on CI:
+BREW=/usr/local/bin/brew
+BAZELISK=/usr/local/bin/bazelisk
+MINT=/usr/local/bin/mint
+
+# # on local:
+# BREW=/opt/homebrew/bin/brew
+# BAZELISK=/opt/homebrew/bin/bazelisk
+# MINT=/opt/homebrew/bin/mint
+
+
+
+
+which brew && which mint && export PATH="" && $BREW install mint && $MINT bootstrap && $BAZELISK build xcodeproj && $BAZELISK build ios-app
--- test.sh
+++ test.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-bazelisk build ios-app && bazelisk build xcodeproj && bazelisk test //Kaly2iOS/... --build_tests_only
+
+#!/bin/bash
+# on CI:
+BREW=/usr/local/bin/brew
+BAZELISK=/usr/local/bin/bazelisk
+MINT=/usr/local/bin/mint
+
+# # on local:
+# BREW=/opt/homebrew/bin/brew
+# BAZELISK=/opt/homebrew/bin/bazelisk
+# MINT=/opt/homebrew/bin/mint
+
+
+export PATH="" && $BAZELISK build ios-app && $BAZELISK build xcodeproj && $BAZELISK test //Kaly2iOS/... --build_tests_only
