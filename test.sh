42b058d4628b580d326a0636e93f94f9b321cd84
Joel Thiessen <jatjat@users.noreply.github.com>
2022-10-22 17:30:45 -0500

    Make SwiftUI previews and command-line presentation tests both work

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bazelisk build $@ ios-app && bazelisk build $@ xcodeproj && bazelisk test $@ //Kaly2iOS/... --build_tests_only
+bazelisk build $@ ios-app && bazelisk build $@ xcodeproj && bazelisk test $@ //:ci_tests --build_tests_only
